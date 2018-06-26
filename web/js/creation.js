80120af8f52051bf3b376fc92e426c6d8cd6bd9d
Elsa <elsa.demaine@gmail.com>
2018-06-26 16:51:02 +0200

    Commit de fin de journées avec error

--- web/js/creation.js
+++ web/js/creation.js
@@ -3,11 +3,25 @@ $(function () {
     var indexContenus = $(".un-contenu").length;
 
     //Collections du formulaire
-    $(document).on("click",".add-contenus", function () {
+    $(document).on("click",".add-text", function () {
 
-        var $prototype = $("#prototype-contenu");
-        console.log($prototype);
+        var $prototype = $("#prototype-contenuText");
+        var prototype = $prototype.data("prototype");
+        var newContenu = prototype.replace(/__name__/g, indexContenus);
 
+        $(".wrap-contenus").append(newContenu);
+        indexContenus++;
+    }).on("click",".add-sousTitre", function () {
+
+        var $prototype = $("#prototype-contenuSousTitre");
+        var prototype = $prototype.data("prototype");
+        var newContenu = prototype.replace(/__name__/g, indexContenus);
+
+        $(".wrap-contenus").append(newContenu);
+        indexContenus++;
+    }).on("click",".add-image", function () {
+
+        var $prototype = $("#prototype-contenuImage");
         var prototype = $prototype.data("prototype");
         var newContenu = prototype.replace(/__name__/g, indexContenus);
 
